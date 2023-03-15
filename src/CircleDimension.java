6b95d01a23eb06cf60cfffbc684c95d9e4855279
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-03-15 09:32:51 +0900

    Dimension of a Circle

---
+++ src/CircleDimension.java
@@ -0,0 +1,8 @@
+public class CircleDimension {
+    public static void main(String[] args) {
+        double radius = 5;
+        double dimension = Math.PI * Math.pow(radius, 2);
+
+        System.out.println(dimension);
+    }
+}
