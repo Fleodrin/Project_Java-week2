ffc8cfe0c71daf16f4b05ad52768db8b68e4117f
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-03-15 09:40:51 +0900

    Revert "Output"
    This reverts commit dfab2a9050f1c7f5fa5968ca3219aec0a27b2ce8.

--- src/BasicOutput.java
+++
@@ -1,8 +0,0 @@
-public class BasicOutput {
-    public static void main(String[] args) {
-        int ival = 3;
-        double dval = 3.14;
-
-        System.out.println("Int value = " + ival + ", Dbl value = " + dval +"\n");
-    }
-}
