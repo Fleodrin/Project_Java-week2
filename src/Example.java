2028416838cb15459c9734051ab7c2e165d832bc
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-03-15 09:25:21 +0900

    Problem #2: Type Conversion

---
+++ src/Example.java
@@ -0,0 +1,12 @@
+public class Example {
+    public static void main(String[] args) {
+        int intNumber;
+        double doubleNumber;
+
+        intNumber = 3 / 4;
+        doubleNumber = 3 / 4;
+
+        System.out.println(intNumber);
+        System.out.println(doubleNumber);
+    }
+}
