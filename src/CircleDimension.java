54091863381ce2eabfaf07348a54e9abf5d85c25
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-03-15 09:38:46 +0900

    Output

--- src/CircleDimension.java
+++ src/CircleDimension.java
@@ -1,8 +1,8 @@
 public class CircleDimension {
     public static void main(String[] args) {
-        double radius = 5;
-        double dimension = Math.PI * Math.pow(radius, 2);
+        int ival = 3;
+        double dval = 3.14;
 
-        System.out.println(dimension);
+        System.out.println("Int value = " + ival + ", Dbl value = " + dval +"\n");
     }
 }
